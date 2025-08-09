9ff5499a78e27aa33d1e5a69669ca08308b8fc8b
rosslinton <128409892+rosslinton@users.noreply.github.com>
2025-08-09 15:23:52 +0100

    Create init-database.sql

---
+++ scripts/init-database.sql
@@ -0,0 +1,17 @@
+/*
+===============
+Create Database and Schemas
+==============
+
+
+*/
+
+
+CREATE DATABASE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
